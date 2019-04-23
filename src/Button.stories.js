e0f4499a854e6c02338ea499d6ae4bc92f975e75
Amol_J <amol.jamkar@centelon.com>
2019-04-23 09:55:07 +0530

    Story file location changes for testing

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Button from './Button';
+import { Button } from './index';
 
 storiesOf('Button')
 	.add('Button', () => (
